d3da2fece386ba29a974773725abf394eae121d7
KirScr <142673005+scrsdk@users.noreply.github.com>
2025-08-29 14:59:20 +0300

    Update gameClient.ts

--- apps/telegram-game/src/utils/gameClient.ts
+++ apps/telegram-game/src/utils/gameClient.ts
@@ -1,6 +1,6 @@
 import { BaseGameAddon } from '@chat-game/game-addon'
 
-const websocketUrl = 'wss://chatgame.space/api/websocket'
+const websocketUrl = 'wss://melvek.ru/api/websocket'
 const gameClient = new BaseGameAddon({ websocketUrl, client: 'TELEGRAM_CLIENT' })
 
 const roomConnected = ref<string | null>(null)
